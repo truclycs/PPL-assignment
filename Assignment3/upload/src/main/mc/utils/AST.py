83e14c909635c15dc83ce1bc80485c1344907aa6
“truclycs” <truclybk.cs@gmail.com>
2019-11-10 10:18:39 +0700

    0

--- Assignment3/upload/src/main/mc/utils/AST.py
+++ Assignment3/upload/src/main/mc/utils/AST.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 from typing import List
 from Visitor import Visitor
 
-
 class AST(ABC):
     def __eq__(self, other): 
         return self.__dict__ == other.__dict__
@@ -291,5 +290,3 @@ class Program(AST):
     
     def accept(self, v: Visitor, param):
         return v.visitProgram(self, param)
-
-
